bf9acaec42816885b231678e567728e6c8531420
Yaphet.Chan <Yaphet.Chern@gmail.com>
2016-12-13 14:25:52 +0800

    Modify the releasetools.py

--- releasetools.py
+++ releasetools.py
@@ -1,8 +1,8 @@
 import common
 import edify_generator
+import os
 
-def ModifyBegin(info):
-  edify = info.script
+def ModifyBegin(edify):
   edify.script[0] = \
   '''ifelse(is_mounted("/system"), unmount("/system"));
 ui_print("******************************************");
@@ -12,16 +12,14 @@ ui_print("* Romer: Po_Chan");
 ui_print("* http://weibo.com/PoChanL");
 ui_print("******************************************");\n''' + edify.script[0]
 
-def AddPrompt(info):
-  edify = info.script
-  for i in xrange(len(edify.script)):
-    if "format(" in edify.script[i] and "by-name/system" in edify.script[i]:
-      edify.script[i] = 'ui_print("{*} Formatting System");\n' + edify.script[i]
-    elif 'set_metadata_recursive("/system"' in edify.script[i]:
-      edify.script[i] = 'ui_print("{*} Setting Permissions");\n' + edify.script[i]
-    elif 'package_extract_file("boot' in edify.script[i]:
-      edify.script[i] = 'ui_print("{*} Flashing Kernel");\n' + edify.script[i]
+def AddPrompt(edify):
+    for i in xrange(len(edify.script)):
+        if "block_image_update" in edify.script[i] and "by-name/system" in edify.script[i]:
+            edify.script[i] = 'ui_print("{*} Extracting System");\n' + edify.script[i]
+        elif 'package_extract_file("boot' in edify.script[i]:
+            edify.script[i] = 'ui_print("{*} Flashing Kernel");\n' + edify.script[i]
 
 def FullOTA_InstallEnd(info):
-    ModifyBegin(info)
-    AddPrompt(info)
+    edify = info.script
+    ModifyBegin(edify)
+    AddPrompt(edify)
